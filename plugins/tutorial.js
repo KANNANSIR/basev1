4b68e1deb916609912ac9518443d0777b36a08b4
KriZ SeR <92368721+KANNANSIR@users.noreply.github.com>
2021-12-27 10:21:32 +0530

    Update tutorial.js

--- plugins/tutorial.js
+++ plugins/tutorial.js
@@ -1,29 +1,9 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   conn.reply(m.chat, `
-Tutorial Buat Bot WA:
-Credit: *~ZyxMapLe*
 
-Ketik di *[TERMUX]*
-1) $ apt update
-2) $ apt upgrade
-3) $ pkg install ffmpeg
-4) $ pkg install imagemagick
-5) $ pkg install nodejs
 
-Untuk SC Nya..
-Cek Di Channel Ku
-6) $ termux-setup-storage
-7) $ cd storage/downloads/(nama file yg kmu rename)
+[ Note : VANNA VAZHI THANNE VITTO]
 
-*Terakhir*..
-Penginstalannya cuy :)
-9 ) $ npm start
-
-[ Note : Tanda $ Ga Perlu Di Ketik ]
-
-*Nanti scnya*..
-
-sabar ya!!
 `.trim(), m)
 }
 handler.help = ['tutorial']
