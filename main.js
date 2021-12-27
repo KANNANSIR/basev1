eff43fdead659bc87b5ec5e06baef4f3130ade18
KriZ SeR <92368721+KANNANSIR@users.noreply.github.com>
2021-12-27 09:52:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -120,7 +120,7 @@ global.reloadHandler = function () {
     conn.off('group-participants-update', conn.onParticipantsUpdate)
     conn.off('CB:action,,call', conn.onCall)
   }
-  conn.welcome = 'Hai @user\n◪ Welcome in group:\n├─ @subject\n\n├─ Intro dulu\n├─ ❏ Nama: \n├─ ❏ Umur: \n├─ ❏ Asal kota: \n├─ ❏ Kelas: \n├─ ❏ Jenis kelamin: \nFOLLOW AKUN OWNER\n➣https://www.instagram.com/farrellatharr_/\n\nSUBSCRIBE YT OWNER\n➣ https://youtube.com/channel/UCDouO-m56kRAr8QzOqfUIYA'
+  conn.welcome = 'Hai @user\n◪ Welcome in group:\n├─ @subject\n\n├─ Intro dulu\n├─ ❏ Nama: \n├─ ❏ Umur: \n├─ ❏ Asal kota: \n├─ ❏ Kelas: \n├─ ❏ Jenis kelamin: \nFOLLOW AKUN OWNER\n➣https://www.instagram.com/kannanbro_f_f/\n\nSUBSCRIBE YT OWNER\n➣ https://youtube.com/channel/UCDo'
   conn.bye = 'Selamat Tinggal @user!\nTerimakasih Telah Bergabung Di Grup\n\nKalo Balik Jangan Lupa Bawa Gorengan Buat Anggota Disini'
   conn.spromote = '@user Sekarang Admin!'
   conn.sdemote = '@user Sekarang Bukan Admin!'
