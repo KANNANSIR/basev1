232d01ac158bcce35b9d81cbea70561cc3181af9
KriZ SeR <92368721+KANNANSIR@users.noreply.github.com>
2021-12-27 09:55:30 +0530

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,7 @@
 let handler = async m => m.reply(`
-┏━━°❀❬ *DONASI* ❭❀°━━┓
-┣➥ *Three:* [0895-3782-64836]
-┣➥ *Gopay:* [0895-3782-64836]
+
 ┃ 「 *Chat OWNER* 」
-┃ > *Ingin donasi? Wa.me/62895378264836*
+┃ > *Ingin donasi? Wa.me/919633687665*
 ┗━━━━━━━━━━━━━━━━
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
